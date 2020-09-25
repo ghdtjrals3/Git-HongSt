c94a1a7e35b3a32d33ec33fa6fc85deed3450722
ghdtjrals3 <ghdtjrals3@gmail.com>
2020-09-25 18:17:27 +0900

    프로그래머스[문자열정수로바꾸기]

---
+++ 프로그래머스(잔디심기)/programus17.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class programus17 {
+
+	public static void main(String[] args) {
+		String s = "1234";
+		programus17 quest = new programus17();
+	    quest.solution(s);
+
+	}
+	 public int solution(String s) {
+	        int answer = 0;
+	        int num = 0;
+	        num = Integer.valueOf(s);
+	        answer = num;
+	        return answer;
+	    }
+
+}
