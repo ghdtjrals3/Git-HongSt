59137a88a1190484833796dc2c052123a64d87b5
ghdtjrals3 <ghdtjrals3@gmail.com>
2020-11-19 09:38:46 +0900

    2020-11-19

--- 소켓프로그래밍 공부/socket/Server/src/MainServer.java
+++ 소켓프로그래밍 공부/socket/Server/src/MainServer.java
@@ -12,7 +12,7 @@ public class MainServer {
 			ServerSocket s_socket = new ServerSocket(8888);
 			
 			//클라이언트가 해당 포트로 연결을 시도한다면 aceept는 대기를풀고
-			//클라이언트와 연결시키는 Socket 클래스를 생성하여 반환함.
+			//클라이언트와 연결시키는 "Socket" 클래스를 생성하여 반환함.
 			Socket c_socket = s_socket.accept();
 			
 			OutputStream output_data = c_socket.getOutputStream();
