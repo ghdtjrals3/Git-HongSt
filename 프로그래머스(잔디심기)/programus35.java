de37ec9199177fe6b06dde42f5525199c991b2df
ghdtjrals3 <ghdtjrals3@gmail.com>
2020-11-25 09:41:39 +0900

    프로그래머스[예산]

---
+++ 프로그래머스(잔디심기)/programus35.java
@@ -0,0 +1,29 @@
+package programus;
+
+import java.util.Arrays;
+
+public class programus35 {
+
+	public static void main(String[] args) {
+		int [] d = {1,3,2,5,4};
+		int budget = 9;
+		programus35 quest = new programus35();
+		quest.solution(d, budget);
+
+	}
+	public int solution(int[] d, int budget) {
+        int answer = 0;
+		int sum = 0;
+		Arrays.sort(d);
+		
+		for(int i=0; i<d.length; i++) { 
+			sum += d[i];
+			if(sum <= budget) {
+				answer ++;
+			}
+		}
+		
+		return answer;
+    }
+
+}
