3c55c54763c17001e487f9a38cc32d4d797e8caa
ghdtjrals3 <ghdtjrals3@gmail.com>
2020-11-27 09:47:12 +0900

    2020-11-27

--- 소켓프로그래밍 공부/socket/Client/src/MainClient.java
+++ 소켓프로그래밍 공부/socket/Client/src/MainClient.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 
 public class MainClient {
@@ -7,7 +9,17 @@ public class MainClient {
 			//Socket 객체를 생성하여 연결을 시도함
 			//연결할 IP주소와 Port번호를 매개변수로 넘겨주어야 해당 주소로 연결함.
 			Socket c_socket = new Socket("192.168.0.2", 8888);
-		}catch (Exception e) {
+			
+			InputStream input_data = c_socket.getInputStream();
+			
+			byte[] receiveBuffer = new byte[100];
+			input_data.read(receiveBuffer);
+			
+			System.out.println(new String(receiveBuffer));
+			
+			c_socket.close();
+		}catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
