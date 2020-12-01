8627637b662f060612968e5611bb9616843faf88
ghdtjrals3 <ghdtjrals3@gmail.com>
2020-12-01 10:08:01 +0900

    2020-12-01

--- 소켓프로그래밍 공부/socket/Client/src/MainClient.java
+++ 소켓프로그래밍 공부/socket/Client/src/MainClient.java
@@ -10,6 +10,8 @@ public class MainClient {
 			//연결할 IP주소와 Port번호를 매개변수로 넘겨주어야 해당 주소로 연결함.
 			Socket c_socket = new Socket("192.168.0.2", 8888);
 			
+			
+			//만약 연결이 완료 되었다면 데이터를 스트림 형태로 받아와서 콘솔에 출력
 			InputStream input_data = c_socket.getInputStream();
 			
 			byte[] receiveBuffer = new byte[100];
